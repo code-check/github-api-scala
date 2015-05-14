dd87cd485b3d2c4958c106be34838e4d40bbd635
sukeshni <sukeshni.kantrod@gmail.com>
2015-05-14 17:22:23 +0530

    fixed UserInput#apply

--- src/main/scala/codecheck/github/models/User.scala
+++ src/main/scala/codecheck/github/models/User.scala
@@ -26,20 +26,3 @@ case class UserInput (
 	hireable: Option[String] = None,
 	bio: Option[String] = None
 ) extends AbstractInput
-
-object UserInput {
-  def apply(name: String): UserInput =
-    UserInput(Some(name), None, None, None, None, None, None)
-  def apply(name: String, email: String): UserInput =
-    UserInput(Some(name), Some(email), None, None, None, None, None)
-  def apply(name: String, email: String,blog: String): UserInput =
-    UserInput(Some(name), Some(email), Some(blog), None, None, None, None)
-  def apply(name: String, email: String,blog: String,company: String): UserInput =
-    UserInput(Some(name), Some(email), Some(blog), Some(company), None, None, None)
-  def apply(name: String, email: String,blog: String,company: String, location: String): UserInput =
-    UserInput(Some(name), Some(email), Some(blog), Some(company), Some(location), None, None)
-  def apply(name: String, email: String,blog: String,company: String, location: String, hireable: String): UserInput =
-    UserInput(Some(name), Some(email), Some(blog), Some(company), Some(location), Some(hireable), None)
-  def apply(name: String, email: String,blog: String,company: String, location: String, hireable: String, bio: String): UserInput =
-    UserInput(Some(name), Some(email), Some(blog), Some(company), Some(location), Some(hireable), Some(bio))
-}
