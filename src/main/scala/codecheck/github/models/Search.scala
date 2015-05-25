6b483bf053973cb47bc56b4698e32702e65f6447
sukeshni <sukeshni.kantrod@gmail.com>
2015-05-25 10:05:21 +0530

    searchCode, searchIssues, searchUser

--- src/main/scala/codecheck/github/models/Search.scala
+++ src/main/scala/codecheck/github/models/Search.scala
@@ -2,15 +2,28 @@ package codecheck.github.models
 
 import org.json4s.JValue
 import org.json4s.JArray
-import codecheck.github.models.SortDirection
 
 sealed abstract class SearchSort(val name: String) {
  override def toString = name
 }
 object SearchSort {
- case object stars extends SearchSort("stars")
- case object forks extends SearchSort("forks")
- case object updated extends SearchSort("updated")
+  //for serachRepositories
+  case object stars extends SearchSort("stars")
+  case object forks extends SearchSort("forks")
+  case object updated extends SearchSort("updated")
+
+  //for searchCode
+  case object indexed extends SearchSort("indexed")
+
+  //for searchIssues
+  case object comments extends SearchSort("comments")
+  case object created extends SearchSort("created")
+  //case object updated extends SearchSort("updated")
+
+  //for searchUser
+  case object followers extends SearchSort("followers")
+  case object repositories extends SearchSort("repositories")
+  case object joined extends SearchSort("joined")
 }
 
 case class SearchInput (
@@ -24,6 +37,38 @@ case class SearchRepositoryResult(value: JValue) extends AbstractJson(value) {
  def incomplete_results: Boolean = boolean("incomplete_results")
  lazy val items = (value \ "items") match {
     case JArray(arr) => arr.map(new Repository(_))
+    case _ => Nil
+  }
+}
+
+case class searchCodeItems (value: JValue) extends AbstractJson(value){
+  def name: String = get("name")
+  lazy val Repo = new Repository(value \ "repository")
+}
+
+case class SearchCodeResult(value: JValue) extends AbstractJson(value) {
+ def total_count: Long = get("total_count").toLong
+ def incomplete_results: Boolean = boolean("incomplete_results")
+ lazy val items = (value \ "items") match {
+    case JArray(arr) => arr.map(new searchCodeItems(_))
+    case _ => Nil
+  }
+}
+
+case class SearchIssueResult(value: JValue) extends AbstractJson(value) {
+ def total_count: Long = get("total_count").toLong
+ def incomplete_results: Boolean = boolean("incomplete_results")
+ lazy val items = (value \ "items") match {
+    case JArray(arr) => arr.map(new Issue(_))
+    case _ => Nil
+  }
+}
+
+case class SearchUserResult(value: JValue) extends AbstractJson(value) {
+ def total_count: Long = get("total_count").toLong
+ def incomplete_results: Boolean = boolean("incomplete_results")
+ lazy val items = (value \ "items") match {
+    case JArray(arr) => arr.map(new User(_))
     case _ => Nil
   }
 }
--- src/main/scala/codecheck/github/operations/SearchOp.scala
+++ src/main/scala/codecheck/github/operations/SearchOp.scala
@@ -6,6 +6,10 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import codecheck.github.api.GitHubAPI
 import codecheck.github.models.SearchInput
 import codecheck.github.models.SearchRepositoryResult
+//import codecheck.github.models.SortDirection
+import codecheck.github.models.SearchCodeResult
+import codecheck.github.models.SearchIssueResult
+import codecheck.github.models.SearchUserResult
 
 trait SearchOp {
  self: GitHubAPI =>
@@ -18,5 +22,35 @@ trait SearchOp {
         case 404 => None
      }
    }
+  }
+
+  def searchCode(input: SearchInput): Future[Option[SearchCodeResult]] = {
+    val path = s"/search/code?q=${input.q}&sort=${input.sort}&order=${input.order}"
+    exec("GET", path ).map { res =>
+      res.statusCode match {
+        case 200 => Some(SearchCodeResult(res.body))
+        case 404 => None
+     }
+    }
+  }
+
+  def searchIssues(input: SearchInput): Future[Option[SearchIssueResult]] = {
+    val path = s"/search/issues?q=${input.q}&sort=${input.sort}&order=${input.order}"
+    exec("GET", path ).map { res =>
+      res.statusCode match {
+        case 200 => Some(SearchIssueResult(res.body))
+        case 404 => None
+     }
+    }
+  }
+
+  def searchUser(input: SearchInput): Future[Option[SearchUserResult]] = {
+    val path = s"/search/user?q=${input.q}&sort=${input.sort}&order=${input.order}"
+    exec("GET", path ).map { res =>
+      res.statusCode match {
+        case 200 => Some(SearchUserResult(res.body))
+        case 404 => None
+     }
+    }
   }
 }
