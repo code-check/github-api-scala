d3da76b3f44b7a413a0579b30e779db8241ac2ba
da0shi <whit3.develop@gmail.com>
2015-09-03 16:36:02 +0900

    Email might be null when user set their email as private

--- src/main/scala/codecheck/github/models/User.scala
+++ src/main/scala/codecheck/github/models/User.scala
@@ -7,7 +7,7 @@ import org.json4s.JsonDSL._
 case class User(value: JValue) extends AbstractJson(value) {
   def login: String = get("login")
   def id: Long = get("id").toLong
-  def email: String = get("email")
+  def email: Option[String] = opt("email")
   def name: String = get("name")
   def blog: String = get("blog")
   def company: String = get("company")
